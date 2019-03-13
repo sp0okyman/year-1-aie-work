a677df251166b188b114404f7c917d4edb42740c
Devran.James.Yilmaz <devran.james.yilmaz@gmail.com>
2019-03-13 11:34:14 +1100

    Finished Number Guessing Game
    It was a lot easier than it looked.

--- Assessment Task 03 - Number Guessing Game/Assessment Task 03 - Number Guessing Game/main.cpp
+++ Assessment Task 03 - Number Guessing Game/Assessment Task 03 - Number Guessing Game/main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 
-void guess();
-void user_inquiry();
+// declaring function
+int binary_search(int a_array[], int a_left, int a_right, int a_turns);
 
 int main()
 {
 	char user_input;
-	
+	bool guess_correct;
 
-	guess();
-	user_inquiry();
-
-	system("pause");
-
-	return 0;
-}
-
-void guess()
-{
 	const int array_size = 100;
 	int possible_guesses[array_size];
 
@@ -25,9 +15,46 @@ void guess()
 	{
 		possible_guesses[index] = index + 1;
 	}
+
+	binary_search(possible_guesses, 0, array_size, 0);
+
+	system("pause");
+
+	return 0;
 }
 
-//void user_inquiry()
-//{
-//	std::cout << "Is your number greater or less than " <<  << '\n';
-//}
+// defining binary_search
+int binary_search(int a_array[], int a_left, int a_right, int a_turns)
+{
+	int mid = a_left + (a_right - a_left) / 2;
+	char user_input;
+
+	std::cout << "Is your number greater than, less than or equal to " << mid << std::endl;
+	std::cin >> user_input;
+
+	if (a_right >= a_left)
+	{
+		switch (user_input) 
+		{
+		case '=':
+			std::cout << "Your number was " << mid << std::endl;
+			std::cout << "Turns taken = " << a_turns + 1 << std::endl;
+			return mid;
+			break;
+		case '>':
+			++a_turns;
+			return binary_search(a_array, a_left, mid - 1, a_turns);
+			break;
+
+		case '<':
+			++a_turns;
+				return binary_search(a_array, mid + 1, a_right, a_turns);
+				break;
+		default:
+			std::cout << "That is not a valid input, input the symbols '>', '<' or '=' to represent your answer." << std::endl;
+			return binary_search(a_array, a_left, a_right, a_turns);
+			break;
+		}
+	}
+	return -1;
+}
